38615b6a0120125fde24b1386b873d2ba4e7e850
Yonatan Ellman <yonatan@nodeside.com>
2014-03-27 16:21:21 +0200

    added auth.

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,21 +6,21 @@ var tokens = require('../controllers/tokens');
 module.exports = function(Tokens, app, auth) {
 
     app.get('/tokens', tokens.all);
-    app.post('/tokens', requiresAdmin, tokens.create);
+    app.post('/tokens', auth.requiresAdmin, tokens.create);
     app.get('/tokens/id/:tokenId', tokens.show);
-    app.put('/tokens/id/:tokenId', requiresAdmin, tokens.update);
-    app.del('/tokens/id/:tokenId', requiresAdmin, tokens.destroy);
+    app.put('/tokens/id/:tokenId', auth.requiresAdmin, tokens.update);
+    app.del('/tokens/id/:tokenId', auth.requiresAdmin, tokens.destroy);
     
     app.get('/tokens/title', tokens.all);
-    app.post('/tokens/title/:title', requiresAdmin, tokens.create);
+    app.post('/tokens/title/:title', auth.requiresAdmin, tokens.create);
     app.get('/tokens/title/:title', tokens.search);
-    app.put('/tokens/title/:title', requiresAdmin, tokens.updateByTitle);
-    app.del('/tokens/title/:title', requiresAdmin, tokens.destroyByTitle);
+    app.put('/tokens/title/:title', auth.requiresAdmin, tokens.updateByTitle);
+    app.del('/tokens/title/:title', auth.requiresAdmin, tokens.destroyByTitle);
     
     app.get('/tokens/category', tokens.all);
-    app.post('/tokens/category/:category', requiresAdmin, tokens.create);
+    app.post('/tokens/category/:category', auth.requiresAdmin, tokens.create);
     app.get('/tokens/category/:category', tokens.all);
-    app.put('/tokens/category/:category', requiresAdmin, tokens.updateByCategory);
+    app.put('/tokens/category/:category', auth.requiresAdmin, tokens.updateByCategory);
 
     // Finish with setting up the tokenId param
     app.param('tokenId', tokens.token);
